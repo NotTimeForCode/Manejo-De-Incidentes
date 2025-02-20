a3775d75b979c60972f80c7645eccda1a220eca6
Esa <esa.aaro.pistemaa@gmail.com>
2025-02-20 21:10:47 +0100

    agregada SQL_declaraciones.sql

---
+++ SQL_declaraciones.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS `incidents` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
+USE `incidents`;
+
+CREATE TABLE IF NOT EXISTS `incident-logs` (
+  `hostname` varchar(50) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `incident_id` int(11) NOT NULL AUTO_INCREMENT,
+  `incident` varchar(255) NOT NULL,
+  `log_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
